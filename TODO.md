be13f9b1f64bbcb1ba3db10cf3be518fbe8b7e58
Alex <zamolots@gmail.com>
2023-03-20 17:14:35 +0000

    Moving Tool to Tools -> Internal Actions -> UI -> Demos. Adding TODO.

---
+++ TODO.md
@@ -0,0 +1,10 @@
+# Internship project TODO
+
+> Create Plugin - X \
+> Create Popup - X \
+> Move to Correct place - X \
+> Create Buttons in Popup - \
+> Connect to internal web-browser - \
+> Create Button functionality - \
+> Create Tests - \
+> Pretty up and Final checks -  
--- src/main/java/com/example/demo/PopupDialogAction.java
+++ src/main/java/com/example/demo/PopupDialogAction.java
@@ -2,6 +2,10 @@ package com.example.demo;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.pom.Navigatable;
 import org.jetbrains.annotations.NotNull;
 
 public class PopupDialogAction extends AnAction {
@@ -10,18 +14,23 @@ public class PopupDialogAction extends AnAction {
 
     @Override
     public void update(@NotNull AnActionEvent event) {
-        // Using the event, evaluate the context,
-        // and enable or disable the action.
+        Project currentProject = event.getProject();
+        event.getPresentation().setEnabledAndVisible(currentProject != null);
     }
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent event) {
-        // Using the event, implement an action.
-        // For example, create and show a dialog.
-        this.event = event;
+        // Basic Popup to test if action works
+
+        Project currentproject = event.getProject();
+        String message = event.getPresentation().getText();
+        String title = event.getPresentation().getDescription();
+        Messages.showMessageDialog(
+                currentproject,
+                message,
+                title,
+                Messages.getInformationIcon()
+        );
     }
-
-
-    // Override getActionUpdateThread() when you target 2022.3 or later!
 
 }
